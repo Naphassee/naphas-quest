b789fa3f6b7ecda919b06e0031ab24d7f09c0fb2
Naphas Seenakasa <naphazze@Naphass-MacBook-Air.local>
2025-06-19 10:59:55 +0700

    Refactor CI configuration, enhance Todo creation with Turbo Streams, and add Rubocop

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -19,8 +19,7 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
     assert_difference("Todo.count") do
       post todos_url, params: { todo: { done: @todo.done, title: @todo.title } }
     end
-
-    assert_redirected_to todo_url(Todo.last)
+    assert_redirected_to todos_url
   end
 
   test "should show todo" do
